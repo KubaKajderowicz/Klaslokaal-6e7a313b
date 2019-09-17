dbd510417e08efd3dadb6d2626a9f342dd579b7f
KubaKajderowicz <2115394@talnet.nl>
2019-09-17 14:26:48 +0200

    Uitwerking van de exercise

---
+++ klaslokaal.php
@@ -0,0 +1,17 @@
+<?php
+
+$var1 = "Sjonnie de Wiel";
+$var2 = "Herman Kaal";
+$var3 = "Henk de Steen";
+$var4 = "Inge Kerkhoven";
+$var5 = "Gert Kruiswijk";
+
+$array = array(
+    $var1,
+    $var2,
+    $var3,
+    $var4,
+    $var5
+);
+
+var_dump($array);
